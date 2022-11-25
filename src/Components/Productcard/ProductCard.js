500d2d237d5ec5e00448db33680c07b9a2ee510e
Amith0205 <amithharish019@gmail.com>
2022-11-25 22:40:01 +0530

    Routing done, changed few things

--- src/Components/Productcard/ProductCard.js
+++ src/Components/Productcard/ProductCard.js
@@ -12,7 +12,7 @@ function ProductCard() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">View More</Button>
+        <Button variant="dark" href={'/product'} >View More</Button>
       </Card.Body>
     </Card>
   );
